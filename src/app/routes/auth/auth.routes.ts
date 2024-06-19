be03fde013e7fc0c436d2994d5e7d7a3ead91751
streamworlddev <streamworld15@gmail.com>
2024-06-19 20:40:13 +0300

    add login canActivate guard

--- src/app/routes/auth/auth.routes.ts
+++ src/app/routes/auth/auth.routes.ts
@@ -3,10 +3,13 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { securedRouteGuard } from '../../core/auth/guards/secured-route.guard';
 import { AuthRoles } from '../../core/auth/constants/auth-roles';
 import { MyAccountPageComponent } from './my-account-page/my-account-page.component';
+import { AuthService } from '../../core/auth/services/auth.service';
+import { inject } from '@angular/core';
 
 export const authRoutes: Routes = [
   {
     path: 'auth/login', // localhost:4200/auth/login
+    canActivate: [()=>!(inject(AuthService).isAuthenticated)],
     component: LoginPageComponent,
   },
   {
