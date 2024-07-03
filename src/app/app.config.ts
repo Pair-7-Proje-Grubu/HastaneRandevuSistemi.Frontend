7792e343ec04b622291a1bf00a9707f98016783d
streamworlddev <streamworld15@gmail.com>
2024-07-03 12:40:47 +0300

    refactor: add locale time config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,12 +7,19 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { loadingInterceptor } from './core/loading-overlay/interceptors/loading.interceptor';
 import { authInterceptor } from './core/auth/intercepters/auth.interceptor';
 import { errorHandlerInterceptor } from './core/auth/intercepters/error-handler.interceptor';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 
+import { registerLocaleData } from '@angular/common';
+import localeTr from '@angular/common/locales/tr';
 
+registerLocaleData(localeTr, 'tr');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), 
-    provideHttpClient(
-      withInterceptors([loadingInterceptor,authInterceptor,errorHandlerInterceptor])), 
-      provideAnimationsAsync()]
+  
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(), 
+    provideHttpClient(withInterceptors([loadingInterceptor,authInterceptor,errorHandlerInterceptor])), 
+    { provide: LOCALE_ID, useValue: 'tr' },
+  ]
 };
